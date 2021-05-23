0c465773863c743ece2efb34b81c67e13aa52ac7
DavidPayet <davidpayet570@gmail.com>
2021-05-23 23:29:13 +0200

    first commit

---
+++ script/app.js
@@ -0,0 +1,254 @@
+// Loader
+const loader = document.querySelector('.loader');
+
+// Home Animation
+const homeName = document.querySelector('.my-name');
+const homeTitle = document.querySelector('h1');
+const HomeSbTitle = document.querySelector('h2');
+const navBtn = document.querySelector('.navigation-icon');
+const logo = document.querySelector('.logo');
+const homeScrollDownBtn = document.querySelector('.scroll-down a span');
+const homeCtaBtn = document.querySelectorAll('.cta-btn');
+const portfolioAnchors = document.querySelectorAll('.portfolio-anchor');
+const contactAnchor = document.querySelector('.contact-anchor');
+const tlHome = gsap.timeline({ paused: true });
+
+tlHome
+  .from(homeName, { y: -50, autoAlpha: 0, ease: 'power3.out', duration: 1 })
+  .from(logo, { x: -50, autoAlpha: 0, rotationY: 720, ease: 'power3.out', duration: 1 }, '-=1')
+  .from(navBtn, { x: 150, autoAlpha: 0, rotation: 720, ease: 'power3.out', duration: 1 }, '-=1')
+  .from(homeTitle, { scaleX: 1.05, autoAlpha: 0, ease: 'power3.out', duration: 1 })
+  .from(HomeSbTitle, { scaleY: -2, autoAlpha: 0, ease: 'power3.out', duration: 1 }, '-=1')
+  .staggerFrom(homeCtaBtn, 1, { autoAlpha: 0 }, .2,)
+  .from(homeScrollDownBtn, { autoAlpha: 0, ease: 'power3.out', duration: 1.5 }, '-=.4')
+
+
+portfolioAnchors.forEach(portfolioAnchor => portfolioAnchor.addEventListener('click', () => {
+  window.location.href = '#portfolio';
+}))
+
+contactAnchor.addEventListener('click', () => {
+  window.location.href = '#contact';
+})
+
+
+const controller = new ScrollMagic.Controller();
+
+// About Animation
+const aboutSection = document.querySelector('#about');
+const aboutTitle = document.querySelector('#about h3');
+const aboutLeftSide = document.querySelector('.profil-img');
+const aboutRigthSide = document.querySelectorAll('#about p');
+
+const tlAbout = gsap.timeline();
+
+tlAbout
+  .from(aboutTitle, { y: -200, opacity: 0, zIndex: -1, duration: 1 })
+  .from(aboutLeftSide, { x: -50, opacity: 0, duration: 1 }, '-=.5')
+  .staggerFrom(aboutRigthSide, 1, { opacity: 0 }, .2, '-=.5')
+
+
+const aboutScene = new ScrollMagic.Scene({
+  triggerElement: aboutSection,
+  triggerHook: .9,
+  reverse: true
+})
+  .setTween(tlAbout)
+  .addTo(controller)
+
+
+// Portfolio Section & Animation
+const portfolioContainer = document.querySelector('.portfolio-container');
+
+const fetchProjects = () => {
+  fetch('../../data/projects.json')
+    .then(res => res.json())
+    .then(projects => {
+
+      projects.map(project => {
+
+        let card = document.createElement('div');
+
+        card.classList = 'card';
+        card.id = project.id;
+        card.innerHTML = `
+            <div class="face-card front">
+              <span class="card-title">${project.title}</span>
+              <img src=${project.front_img} />
+            </div>
+            <div class="face-card back">
+              <img src=${project.back_img} />
+              <div class="icones">
+                <a href="#" target="_blank">
+                  <i class="fas fa-search-plus" data-wenk="Voir le Projet"></i>
+                </a>
+                <a href=${project.github_source_code} target="_blank">
+                  <i class="fab fa-github" data-wenk="Code Source"></i>
+                </a>
+              </div>
+            </div>`;
+
+        portfolioContainer.appendChild(card);
+
+      })
+
+    })
+    .catch(error => {
+      console.error('Something went wrong with retrieving projects !');
+      console.error(error);
+    })
+}
+fetchProjects();
+
+
+const portfolioSection = document.querySelector('#portfolio');
+const portfolioTitle = document.querySelector('#portfolio h3');
+const portfolioCards = document.querySelector('.portfolio-container');
+
+const tlPortfolio = gsap.timeline();
+
+tlPortfolio
+  .from(portfolioTitle, { y: -200, opacity: 0, zIndex: -1, duration: 1 })
+  .from(portfolioCards, { opacity: 0, duration: 1 })
+  // .staggerFrom(portfolioCards, 1, { opacity: 0 }, .2, '-=.5')
+
+
+const portfolioScene = new ScrollMagic.Scene({
+  triggerElement: portfolioSection,
+  triggerHook: .9,
+  reverse: true
+})
+  .setTween(tlPortfolio)
+  .addTo(controller)
+
+
+// Parallax Section
+const parallaxScene = new ScrollMagic.Scene({
+  triggerElement: '.parallax',
+  triggerHook: 1,
+  duration: '150%',
+  reverse: true
+})
+  .setTween(gsap.from('.bcg', { y: '-60%', ease: Power0.easeNone, duration: 1 }))
+  .addTo(controller);
+
+
+// Random Quotes & Anim
+const texts = document.querySelector('.contenu p');
+const authors = document.querySelector('.contenu span');
+const contenu = document.querySelector('.contenu');
+
+const tlQuote = gsap.timeline({ paused: true });
+
+tlQuote
+  .from(contenu, { scale: 2, opacity: 0, ease: 'power3.out', duration: 1 })
+
+const displayQuote = () => {
+
+  let index = Math.floor(Math.random() * data.length);
+  let quote = data[index].text;
+  let author = data[index].author;
+
+  if (!author) {
+    author = "Anonymous";
+  }
+
+  texts.innerText = quote;
+  authors.innerText = author;
+
+}
+
+
+const fetchQuote = () => {
+  fetch("https://type.fit/api/quotes")
+    .then(res => res.json())
+    .then(function (data) {
+      this.data = data
+
+      setInterval(() => {
+        displayQuote()
+        tlQuote.play()
+        tlQuote.restart()
+      }, 10000);
+    })
+    .catch(error => {
+      console.error('Something went wrong with retrieving quotes !');
+      console.error(error);
+    })
+}
+fetchQuote();
+
+
+// Contact Section Animation
+const inputFields = document.querySelectorAll('input');
+
+for (let i = 0; i < inputFields.length; i++) {
+  let field = inputFields[i];
+
+  field.addEventListener('input', (e) => {
+    if (e.target.value !== "") {
+      e.target.parentNode.classList.add('animation');
+    } else if (e.target.value === "") {
+      e.target.parentNode.classList.remove('animation');
+    }
+  })
+}
+
+
+const contactSection = document.querySelector('#contact');
+const contactTitle = document.querySelector('#contact h3');
+const contactForm = document.querySelector('.form-bloc');
+const contactInfo = document.querySelector('.contact-info');
+
+const tlContact = gsap.timeline();
+
+tlContact
+  .from(contactTitle, { y: -200, opacity: 0, zIndex: -1, duration: 1 })
+  .from(contactForm, { x: -200, opacity: 0, duration: 1 })
+  .from(contactInfo, { x: 200, opacity: 0, duration: 1 }, '-=1')
+
+
+const contactScene = new ScrollMagic.Scene({
+  triggerElement: contactSection,
+  triggerHook: .9,
+  reverse: true
+})
+  .setTween(tlContact)
+  .addTo(controller)
+
+
+// Paper Plane Anim
+const plane = document.querySelector('#plane-anim');
+const submitBtn = document.querySelector('.submit-btn');
+const path = "M1 59.5L539.5 210.5L830.5 329.5L953.855 385.28C1025.31 417.592 1058.79 500.332 1029.9 573.24L1027.78 578.596C1009.67 624.296 968.468 656.79 919.807 663.742V663.742C893.504 667.499 866.677 663.526 842.593 652.306L835.146 648.837C700.434 586.079 711.284 391.051 852.124 343.621L973.27 302.824C1019.31 287.319 1069.74 291.769 1112.35 315.097L1245.68 388.08C1306.57 421.415 1381.45 415.363 1436.2 372.683L1913 1";
+
+const tlPlane = new gsap.timeline({ paused: true });
+
+tlPlane
+  .to(plane, {
+    motionPath: {
+      path: path,
+      autoRotate: true,
+    },
+    opacity: 1,
+    ease: 'power1.in',
+    duration: 3,
+    left: -50,
+    scale: 1.5
+  })
+  .to(plane, { opacity: 0, duration: .1, display: 'none' })
+
+
+submitBtn.addEventListener('click', () => {
+  tlPlane.play();
+  tlPlane.restart();
+})
+
+
+window.addEventListener('load', () => {
+  loader.className += ' hidden';
+  tlHome.play();
+  tlPortfolio.play();
+  tlQuote.play();
+  tlContact.play();
+})
---
+++ script/nav.js
@@ -0,0 +1,94 @@
+const item = document.querySelectorAll('.item');
+const btnNav = document.querySelector('.navigation-icon');
+const containerMenu = document.querySelector('.container-menu');
+const b1 = document.querySelector('.b1');
+const b2 = document.querySelector('.b2');
+const b3 = document.querySelector('.b3');
+const menuItem = document.querySelectorAll('.menu-item');
+let toggle = 0;
+
+// Éclater un mot en lettres (=> 1 lettre par span)
+for (let i = 0; i < item.length; i++) {
+  charming(item[i]);
+}
+
+btnNav.addEventListener('click', () => {
+  if (document.documentElement.clientWidth > 576) {
+
+    if (toggle === 0) {
+      toggle++;
+      gsap.to(b2, { display: 'none', duration: .01 });
+      gsap.to(b1, { top: 0, rotation: 225, duration: .1 });
+      gsap.to(b3, { top: -3, rotation: -225, duration: .1 });
+      gsap.to(containerMenu, { left: 0, ease: 'power4.easeInOut', width: '100%', height: '100%', duration: .5, opacity: 1 });
+    } else {
+      toggle--;
+      gsap.to(b2, { delay: .25, display: 'block', duration: .01 });
+      gsap.to(b1, { top: -10, rotation: 0, duration: .1 });
+      gsap.to(b3, { top: 10, rotation: 0, duration: .1 });
+      gsap.to(containerMenu, { left: '-100%', ease: 'power4.easeIn', width: 0, height: 0, duration: .5, opacity: 0 });
+    }
+
+  } else {
+
+    if (toggle === 0) {
+      toggle++;
+      gsap.to(b2, { display: 'none', duration: .01 });
+      gsap.to(b1, { top: 0, rotation: 225, duration: .1 });
+      gsap.to(b3, { top: -2, rotation: -225, duration: .1 });
+      gsap.to(containerMenu, { left: 0, ease: 'power4.easeInOut', width: '100%', height: '100%', duration: .5, opacity: 1 });
+    } else {
+      toggle--;
+      gsap.to(b2, { delay: .25, display: 'block', duration: .01 });
+      gsap.to(b1, { top: -5, rotation: 0, duration: .1 });
+      gsap.to(b3, { top: 5, rotation: 0, duration: .1 });
+      gsap.to(containerMenu, { left: '-100%', ease: 'power4.easeIn', width: 0, height: 0, duration: .5, opacity: 0 });
+    }
+    
+  }
+})
+
+menuItem.forEach(item => item.addEventListener('mouseenter', (e) => {
+  // console.log(e.target.childNodes);
+
+  let letterFromItem = Array.from(e.target.childNodes[1].querySelectorAll('span'));
+  // console.log(letterFromItem);
+
+
+  // Effet d'éclatement
+  for (let i = 0; i < letterFromItem.length; i++) {
+    gsap.to(letterFromItem[i], {
+      x: Math.floor(Math.random() * 100 - 50),
+      y: Math.floor(Math.random() * 100 - 50),
+      z: Math.floor(Math.random() * 100 - 50),
+      rotation: Math.floor(Math.random() * 100 - 50),
+      opacity: .3,
+      ease: 'circ.easeOut',
+      duration: 1
+    })
+  }
+
+  // Effet de replacement
+  menuItem.forEach(item => item.addEventListener('mouseleave', () => {
+    for (let j = 0; j < letterFromItem.length; j++) {
+      gsap.to(letterFromItem[j], {
+        x: 0,
+        y: 0,
+        z: 0,
+        rotation: 0,
+        opacity: 1,
+        ease: 'circ.easeOut',
+        duration: 1
+      })
+    }
+  }))
+
+}))
+
+menuItem.forEach(item => item.addEventListener('click', () => {
+  toggle--;
+  gsap.to(b2, { delay: .25, display: 'block', duration: .01 });
+  gsap.to(b1, { top: -10, rotation: 0, duration: .1 });
+  gsap.to(b3, { top: 10, rotation: 0, duration: .1 });
+  gsap.to(containerMenu, { left: '-100%', ease: 'power4.easeIn', width: 0, height: 0, duration: .5 });
+}))
