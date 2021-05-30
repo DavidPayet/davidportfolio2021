ddc13014a404c8150eca66c1e819304a8d1556f9
DavidPayet <davidpayet570@gmail.com>
2021-05-30 16:31:41 +0200

    remove unnecessary comments

--- script/nav.js
+++ script/nav.js
@@ -1,16 +1,12 @@
-const item = document.querySelectorAll('.item');
 const btnNav = document.querySelector('.navigation-icon');
 const containerMenu = document.querySelector('.container-menu');
 const b1 = document.querySelector('.b1');
 const b2 = document.querySelector('.b2');
 const b3 = document.querySelector('.b3');
+const item = document.querySelectorAll('.item');
 const menuItem = document.querySelectorAll('.menu-item');
 let toggle = 0;
 
-// Éclater un mot en lettres (=> 1 lettre par span)
-for (let i = 0; i < item.length; i++) {
-  charming(item[i]);
-}
 
 btnNav.addEventListener('click', () => {
   if (document.documentElement.clientWidth > 576) {
@@ -19,7 +15,7 @@ btnNav.addEventListener('click', () => {
       toggle++;
       gsap.to(b2, { display: 'none', duration: .01 });
       gsap.to(b1, { top: 0, rotation: 225, duration: .1 });
-      gsap.to(b3, { top: -3, rotation: -225, duration: .1 });
+      gsap.to(b3, { top: -2, rotation: -225, duration: .1 });
       gsap.to(containerMenu, { left: 0, ease: 'power4.easeInOut', width: '100%', height: '100%', duration: .5, opacity: 1 });
     } else {
       toggle--;
@@ -48,14 +44,18 @@ btnNav.addEventListener('click', () => {
   }
 })
 
+
+// Burst a word into letters (=> 1 letter per span)
+for (let i = 0; i < item.length; i++) {
+  charming(item[i]);
+}
+
+
 menuItem.forEach(item => item.addEventListener('mouseenter', (e) => {
-  // console.log(e.target.childNodes);
 
   let letterFromItem = Array.from(e.target.childNodes[1].querySelectorAll('span'));
-  // console.log(letterFromItem);
 
-
-  // Effet d'éclatement
+  // Burst effect
   for (let i = 0; i < letterFromItem.length; i++) {
     gsap.to(letterFromItem[i], {
       x: Math.floor(Math.random() * 100 - 50),
@@ -68,7 +68,7 @@ menuItem.forEach(item => item.addEventListener('mouseenter', (e) => {
     })
   }
 
-  // Effet de replacement
+  // Replacement effect
   menuItem.forEach(item => item.addEventListener('mouseleave', () => {
     for (let j = 0; j < letterFromItem.length; j++) {
       gsap.to(letterFromItem[j], {
